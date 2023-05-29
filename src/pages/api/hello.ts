3e4b3eb9f6bb792b31bf9d4816a32b7eb25f8967
simplysabir <simplysabir@gmail.com>
2023-05-29 09:07:30 +0530

    Updating the Payment URL

---
+++ src/pages/api/hello.ts
@@ -0,0 +1,108 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Connection, Keypair, PublicKey, SystemProgram, Transaction, TransactionConfirmationStrategy } from '@solana/web3.js'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import * as base58 from "base-58";
+type Data = {
+  name: string
+}
+
+type GetData = {
+  label: string
+  icon: string
+}
+
+type PostData = {
+  transaction: string,
+  message?: string
+}
+
+function get(
+  req: NextApiRequest,
+  res: NextApiResponse<GetData>
+  ) {
+  const label = 'Sabir Khan';
+  const icon = 'https://avatars.githubusercontent.com/u/76485932?v=4';
+
+  res.status(200).send({
+      label,
+      icon,
+  });
+}
+
+async function post(
+  req: NextApiRequest,
+  res: NextApiResponse<PostData>
+  ) {
+    // Account provided in the transaction request body by the wallet.
+    const accountField = req.body?.account;
+    if (!accountField) throw new Error('missing account');
+
+    const sender = new PublicKey(accountField);
+
+    const merchant = Keypair.fromSecretKey(
+      new Uint8Array(JSON.parse("[226,230,33,166,183,94,221,240,76,0,177,119,22,166,134,93,69,185,83,121,221,13,229,219,18,55,91,84,86,112,53,87,139,130,97,105,159,216,5,167,211,57,175,154,105,195,156,4,68,100,253,224,35,32,204,44,126,175,226,176,146,254,206,226]")),
+    );
+
+
+    // Build Transaction
+    const ix = SystemProgram.transfer({
+      fromPubkey: sender,
+      toPubkey: new PublicKey("9hFxrJSPps7AoNXkx4feK2gR1FLrbLcCtDiLFswtyyt"),
+      lamports: 133700000
+    })
+
+    let transaction = new Transaction();
+    transaction.add(ix);
+
+    const connection = new Connection("https://api.devnet.solana.com")
+    const bh = await connection.getLatestBlockhash();
+    transaction.recentBlockhash = bh.blockhash;
+    transaction.feePayer = merchant.publicKey; 
+
+    // for correct account ordering 
+    transaction = Transaction.from(transaction.serialize({
+      verifySignatures: false,
+      requireAllSignatures: false,
+    }));
+
+    transaction.sign(merchant);
+    console.log(base58.encode(transaction.signature));
+
+    // airdrop 1 SOL just for fun
+    connection.requestAirdrop(sender, 1000000000);
+
+    // Serialize and return the unsigned transaction.
+    const serializedTransaction = transaction.serialize({
+      verifySignatures: false,
+      requireAllSignatures: false,
+    });
+
+    const base64Transaction = serializedTransaction.toString('base64');
+    const message = 'Thank you for using AndyPay';
+
+    // const strategy : TransactionConfirmationStrategy =  {
+    //   signature: transaction.
+    // }
+    // connection.confirmTransaction();
+
+    res.status(200).send({ transaction: base64Transaction, message });
+
+}
+
+
+
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<GetData|PostData>
+) {
+  if(req.method == "GET"){
+    console.log("Received GET Request");
+    return get(req,res);
+    
+  } else if(req.method == "POST"){
+    console.log("Received POST Request");
+    return await post(req, res);
+  }
+ 
+}
---
+++ src/views/home/index.tsx
@@ -0,0 +1,52 @@
+// Next, React
+import { FC, useEffect, useRef, useState } from 'react';
+import Link from 'next/link';
+
+// Wallet
+import { useWallet, useConnection } from '@solana/wallet-adapter-react';
+
+// Components
+import { RequestAirdrop } from '../../components/RequestAirdrop';
+import pkg from '../../../package.json';
+
+// Store
+import useUserSOLBalanceStore from '../../stores/useUserSOLBalanceStore';
+
+//* Solana Pay 
+const SOLANA_PAY_URL = "solana:http://192.168.13.131:3000/api/hello";
+import {createQR} from "@solana/pay";
+
+
+export const HomeView: FC = ({ }) => {
+
+
+  const qrRef = useRef<HTMLDivElement>()
+  useEffect(() => {
+    const qr = createQR(SOLANA_PAY_URL, 400, 'white', 'black');
+
+    // Set the generated QR code on the QR ref element
+    if (qrRef.current) {
+      qrRef.current.innerHTML = ''
+      qr.append(qrRef.current)
+      console.log("appended");
+    } 
+  }, [])
+
+
+
+  return (
+
+    <div className="md:hero mx-auto p-4">
+      <div className="md:hero-content flex flex-col">
+        <div className='mt-6'>
+        <div className='text-sm font-normal align-bottom text-right text-slate-600 mt-4'>v{pkg.version}</div>
+        <h1 className="text-center text-5xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mb-4">
+          Scan this
+        </h1>
+        <div ref={qrRef}/>
+        </div>
+    </div>
+    </div>
+
+  );
+  }
